758e2f8f6fecd80ee92c4d287d79ad10cb5e1e30
anassovich <ifrah.anass@gmail.com>
2015-03-05 18:03:20 +0100

    First commit

---
+++ src/main/java/dcll/aifr/App.java
@@ -0,0 +1,13 @@
+package dcll.aifr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
