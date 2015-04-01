038b2c4cdb8e0303df3425d0b6efa7e0a798ff02
anassovich <ifrah.anass@gmail.com>
2015-04-01 16:14:05 +0200

    Ajout de la classe de test

---
+++ src/test/java/dcll/aifr/MySimpleStack/SimpleStackImplTest.java
@@ -0,0 +1,38 @@
+package dcll.aifr.MySimpleStack;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SimpleStackImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testIsEmpty() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetSize() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testPush() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testPeek() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testPop() throws Exception {
+
+    }
+}
